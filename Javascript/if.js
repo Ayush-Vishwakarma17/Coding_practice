849b86ad5c8c650dfeea5d3ae0398c4938396601
Ayush <ayushvishhhhh@gmail.com>
2025-11-09 16:08:29 +0530

    js practice

---
+++ Javascript/if.js
@@ -0,0 +1,11 @@
+let num1 = 2
+let num2 = "2"
+
+if (num1 == num2) {
+    console.log("They are equal")
+}
+if (num1 === num2) {
+    console.log("They are strictly equal") 
+} else {
+    console.log("They are not equal");
+}
