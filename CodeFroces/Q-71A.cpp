b0e84e8588cb2c9f69b24e5e6991f1bc68c09549
Ayush <ayushvishhhhh@gmail.com>
2025-11-11 21:16:05 +0530

    daily code done

--- CodeFroces/Q-71A.cpp
+++ CodeFroces/Q-71A.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    vector<string> result;
+    vector<string> ansString;
     int t; cin >> t;
     while (t) {
         t--;
         string input; cin >> input;
         if (input.length() > 10) {
-            char first = input[0];
-            char last = input[input.size()-1];
+            char firstchar = input[0];
+            char lastchar = input[input.size()-1];
             string number = to_string(input.length()-2);
-            result.push_back(first+number+last);
+            ansString.push_back(firstchar+number+lastchar);
         } else {
-            result.push_back(input);
+            ansString.push_back(input);
         }
     }
-    for (auto it: result) {
+    for (auto it: ansString) {
         cout<<it<<endl;
     }
     return 0;
