bbd035f1e066b9753791afbdc30056deb36ac3ae
Ayush <ayushvishhhhh@gmail.com>
2025-11-15 23:26:28 +0530

    done15nov

---
+++ CodeFroces/Q-1901A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin>>t;
+    while (t) {
+        t--;
+        int n, x; cin >> n >> x;
+        vector<int> nums;
+        while (n) {
+            n--;
+            int a;cin>>a;
+            nums.push_back(a);
+        }
+        
+    }
+    return 0;
+}
