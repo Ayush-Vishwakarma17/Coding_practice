59758e4e3be02dbd6facfaedf6115d6b70f8b216
Ayush <ayushvishhhhh@gmail.com>
2025-11-20 22:43:20 +0530

    done

---
+++ java-practice/Class2.java
@@ -0,0 +1,10 @@
+public class Class2 {
+    int x = 17;
+    int b = 19;
+    public static void main(String[] args) {
+        Class2 myobj1 = new Class2();
+        Class2 myobj2 = new Class2();
+        System.out.println("The myobj1 is :" + myobj1.x);
+        System.out.println("The myobj2 is :" + myobj2.b);
+    }
+}
---
+++ java-practice/Methods1.java
@@ -0,0 +1,8 @@
+public class Methods1 {
+    static void method() {
+        System.out.println("Aaaloo leloo!");
+    } 
+    public static void main(String[] args) {
+        method();
+    }
+}
