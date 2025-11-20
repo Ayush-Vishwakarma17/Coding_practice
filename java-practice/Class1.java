50119e8acbaa8e48427fe310cba60f19dc3ff133
Ayush <ayushvishhhhh@gmail.com>
2025-11-20 18:33:32 +0530

    java1

---
+++ java-practice/Class1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Class1 {
+
+    int x = 5;
+
+    public static void main(String[] args) {
+        Class1 obj = new Class1();
+        System.out.println("The object is: " + obj.x);
+
+    }
+}
