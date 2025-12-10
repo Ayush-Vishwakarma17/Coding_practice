6e865ee4ff73dfe0a68b792ac6ba9fb066740520
Ayush <ayushvishhhhh@gmail.com>
2025-12-10 23:07:52 +0530

    cf 1834A done

--- CodeFroces/Q-1834A.cpp
+++ CodeFroces/Q-1834A.cpp
@@ -6,11 +6,13 @@ int main() {
         t--;
         int n; cin>>n;
         vector<int> nums;
+        //making an array to store all ones and negative ones
         while (n) {
             n--;
             int x; cin>>x;
             nums.push_back(x);
         }
+        //initialising neg and pos ones count 
         int neg = 0, pos = 0;
         for (auto val: nums) {
             if (val == -1) {
@@ -19,11 +21,28 @@ int main() {
                 pos++;
             }
         }
+        int ans = 0;
+        //if count of pos and negative ones equal
         if (neg == pos) {
-            cout<<0<<endl;
-        } else if () {
-
+            //making sure the negative ones comes for even times
+            while (neg % 2 != 0) {
+                neg--;
+                pos++;
+                ans++;
+            }
+            cout<<ans<<endl;
+        } // else case if count of ones and negative ones are not equal
+        else {  
+            //making sure the neg ones comes for even times as well as less of equal to 
+            //positive ones
+            while (neg % 2 != 0 || neg > pos) {
+                neg--;
+                pos++;
+                ans++;
+            }
+            cout<<ans<<endl;
         }
     }
+    //returning zero as parampara! ahem ahem
     return 0;
 }
