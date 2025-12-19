7ee48cfafca831f813d964990a271fd30411477c
Ayush <ayushvishhhhh@gmail.com>
2025-12-19 19:26:05 +0530

    reduce done

--- js-practice/reduce.js
+++ js-practice/reduce.js
@@ -1,58 +1,84 @@
 
-//reduce method practice in js
+// //reduce method practice in js
 
-const arr = [
-    {title: "Gekko",
-        hp: 100,
-        pet: "wigman",
-    },
-    {title: "Reyna",
-        hp: 120,
-        pet: "gekko",
-    },
-    {title: "viper",
-        hp: 130,
-        pet: "chamber",
-    },
+// const arr = [
+//     {title: "Gekko",
+//         hp: 100,
+//         pet: "wigman",
+//     },
+//     {title: "Reyna",
+//         hp: 120,
+//         pet: "gekko",
+//     },
+//     {title: "viper",
+//         hp: 130,
+//         pet: "chamber",
+//     },
 
+// ]
+// const newarr = arr.reduce((acc, e) => {
+//     if (e.hp > 100) {
+//         acc.push(e.hp);
+//     }
+//     return acc;
+// }, []);
+
+// for (let val of newarr) {
+//     console.log(val);
+// }
+
+// const books = [
+//     {title: "gulab",
+//         pg: 100,
+//         writer: "prem",
+//     },
+//     {title: "karbala",
+//         pg: 150,
+//         writer: "prem",
+//     },
+//     {title: "gillu",
+//         pg: 120,
+//         writer: "mahadevi",
+//     },
+//     {title: "rachna",
+//         pg: 150,
+//         writer: "mahadevi",
+//     },
+// ]
+
+// const writerNames = books.reduce((acc, key) => {
+//     if (key.writer === "mahadevi") {
+//         acc.push(key.title)
+//     }
+//     return acc
+// }, [])
+
+// for (let name of writerNames) {
+//     console.log(name);
+// }
+
+const movies = [
+    {
+        title: "dhurandar",
+        collection: 500         
+    },
+    {
+        title: "Nezha 2",
+        collection: 1200         
+    },
+    {
+        title: "jawan",
+        collection: 1000        
+    },
 ]
-const newarr = arr.reduce((acc, e) => {
-    if (e.hp > 100) {
-        acc.push(e.hp);
-    }
-    return acc;
-}, []);
 
-for (let val of newarr) {
-    console.log(val);
-}
-
-const books = [
-    {title: "gulab",
-        pg: 100,
-        writer: "prem",
-    },
-    {title: "karbala",
-        pg: 150,
-        writer: "prem",
-    },
-    {title: "gillu",
-        pg: 120,
-        writer: "mahadevi",
-    },
-    {title: "rachna",
-        pg: 150,
-        writer: "mahadevi",
-    },
-]
-
-const writerNames = books.reduce((acc, key) => {
-    if (key.writer === "mahadevi") {
-        acc.push(key.title)
+const blockbuster = movies.reduce((acc, e) => {
+    if (e.collection>=1000) { 
+        acc.push(e)
     }
     return acc
-}, [])
+},[])
 
-for (let name of writerNames) {
-    console.log(name);
+for(let i of blockbuster) {
+    console.log(`${i.title} did ${i.collection} crores of collection by the movie.`)
 }
