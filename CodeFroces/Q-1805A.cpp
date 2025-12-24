891af3e2f0d8c12d0f969ae1daa0b3a27332763f
Ayush <ayushvishhhhh@gmail.com>
2025-12-24 14:29:19 +0530

    cf 1805 done

--- CodeFroces/Q-1805A.cpp
+++ CodeFroces/Q-1805A.cpp
@@ -5,22 +5,33 @@ int main() {
     while (t) {
         t--;
         vector<int> nums;
-        int ans = 0;
+        int totalXor = 0;
         int n; cin>>n;
         while (n) {
             n--;
+            //pushing all elements to nums
             int x; cin>>x;
             nums.push_back(x);
         }
+        //finding the xor of all elements
         for (auto it: nums) {
-            ans^=it;
+            totalXor^=it;
         }
-        if (nums.size() % 2 != 0) {
-            cout<<ans<<endl;
-        } else if (ans == 0) {
-            cout<<0<<endl;
+        //if size is even means we will xor even number of x to every elements
+        //and even x xor will give you 0
+        //so we will get the same xor return that's why if xor zero then print any number
+        //if not zero then print negative one according to question 
+        if (nums.size() % 2 == 0) {
+            if (totalXor == 0) {
+                cout<<1<<endl;
+            } else {
+                cout<<-1<<endl;
+            }
+        //if size is not even means a single x will get xor with total xor
+        //so idea is we will take the x equal to the totalXor 
+        //and two same elements xor gives zero
         } else {
-            cout<<-1<<endl;
+            cout<<totalXor<<endl;
         }
     }
     return 0;
