ad7c4b27bffc937df042be76ecb7b2b7376a96dc
Ayush <ayushvishhhhh@gmail.com>
2025-12-25 23:32:32 +0530

    lc done

--- CodeFroces/Q-1791C.cpp
+++ CodeFroces/Q-1791C.cpp
@@ -2,28 +2,39 @@
 using namespace std; 
 int main() {
     int t; cin>>t;
+
     while (t) {
         t--;
         int n;cin>>n;
         string s; cin>>s;
+
+        //simple two pointer approach to find the zero and one end combo
         int i = 0, j = n-1;
         while (i < j) {
+            //if found combo of zero and one updating i and j
             if (s[i] == '0' && s[j] == '1') {
                 i++;
                 j--;
             } else if (s[i] == '1' && s[j] == '0') {
                i++;
                j--; 
-            } else {
+            } else { //if can't find the combo of zero and one then break the loop
                 break;
             }
         }
+
+        //calculating the length of the string 
+
         int len = 0;
+        //if j pointer index is less than i pointer index means possibly string was empty.
         if (j < i) {
             cout<<len<<endl;
             continue;
         }
+        //calculating length of the string
+
         len = j-i+1;
+        //printing length
         cout<<len<<endl;
     }
     return 0;
