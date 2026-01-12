3412859f187818e3871880da21bbd19807f5712d
Ayush <ayushvishhhhh@gmail.com>
2026-01-12 23:27:59 +0530

    done

---
+++ CodeFroces/Q-4A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin>>t;
+    int div = t/2;
+    if (t > 2 && t % 2 == 0) {
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
