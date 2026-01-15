346e7348bab83035bb899a7f1946814a35fea12f
Ayush <ayushvishhhhh@gmail.com>
2026-01-16 00:00:10 +0530

    done

---
+++ CodeFroces/Q-1875A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin>>t;
+    while (t--) {
+        int a,b,n; cin>>a>>b>>n;
+        vector<int> nums(n, 0);
+        for (int i = 0; i < n; i++) {
+            cin>>nums[i];
+        }
+
+        long long maxsec = b;
+        for (auto it: nums) {
+            maxsec += min(it, a-1);
+        }
+        cout<<maxsec<<endl;
+    }
+    return 0;
+}
