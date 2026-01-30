6defc1e4abe7ad3242a0d47177387a09dfe069ae
Ayush <ayushvishhhhh@gmail.com>
2026-01-31 00:29:34 +0530

    react day 3 done

--- React-01/react-1/src/App.jsx
+++ React-01/react-1/src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
-
+// let counter  = 20
+// const addone = () => {
+//     counter++;
+//     let val = document.querySelector('#counter')
+//     val.innerHTML = `current counter is: ${counter}`
+// }
+// const lessone = () => {
+//     counter--
+//     let val = document.querySelector('#counter')
+//     val.innerHTML = `current counter is: ${counter}`
+// }
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(10)
+  const addone = () => {
+    count += 1
+    setCount(count)
+  }
+  const lessone = () => {
+    count -= 1
+    setCount(count)
+  }
   return (
     <>
-    <div> <h1>CHAI aur React Ayush will be REACT-PRO</h1></div>
+      <div >
+          <h1 id='counter'>current counter is: {count}</h1>
+          <button onClick={addone}>add 1</button>
+          <button onClick={lessone}>less 1</button>
+      </div>
+      
     </>
   )
 }
