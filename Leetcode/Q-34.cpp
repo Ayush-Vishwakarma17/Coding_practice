13e49667a0cb81e59f018080e11c3cbef2aef0e1
Ayush <ayushvishhhhh@gmail.com>
2026-02-22 20:35:55 +0530

    done

--- Leetcode/Q-34.cpp
+++ Leetcode/Q-34.cpp
@@ -19,7 +19,52 @@ public:
     }
 };
 
-------------------- O(log(n)) solution using binary search algorith ------------
+------------------- O(log(n)) solution using binary search algorithm ------------
 
+//we have to binary search the left most target index
+//we have to binary search the right most target index
+ O(2log(n)) ------> O(log(n))
+
+class Solution {
+public:
+int LBS(vector<int>& nums, int target) {
+    int idx = -1;
+    int s = 0, e = nums.size()-1;
+    while (s <= e) {
+        int mid = s + (e-s)/2;
+        if (nums[mid] == target) {
+            idx = mid; 
+            e = mid-1;
+        } else if (nums[mid] > target) {
+            e = mid-1;
+        } else {
+            s = mid +1;
+        }
+    }
+    return idx;
+}
+
+int RBS(vector<int>& nums, int target) {
+    int idx = -1;
+    int s = 0, e = nums.size()-1;
+    while (s <= e) {
+        int mid = s + (e-s)/2;
+        if (nums[mid] == target) {
+            idx = mid; 
+            s = mid+1;
+        } else if (nums[mid] > target) {
+            e = mid-1;
+        } else {
+            s = mid +1;
+        }
+    }
+    return idx;
+}
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        int leftMost = LBS(nums, target), rightMost = RBS(nums,  target);
+        return {leftMost, rightMost};
+    }
+};
 
 */
