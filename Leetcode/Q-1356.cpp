6be355a051c0eaab73b502ec3385e4d503f747a7
Ayush <ayushvishhhhh@gmail.com>
2026-02-25 09:55:13 +0530

    done

---
+++ Leetcode/Q-1356.cpp
@@ -0,0 +1,29 @@
+/*
+
+----------- my solution is pretty straight forward simulating the problem statement ------------
+--------- so yeah this is all i got to solve for today do your best in upcoming days ayushhh keep going forward -------------
+25 feb 2026 peace out! 
+
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<int> result;
+        sort(arr.begin(), arr.end());
+        map<int, vector<int>> mp;
+        for (auto val: arr) {
+            int setbits = __builtin_popcount(val);
+            mp[setbits].push_back(val);
+        }
+        for (auto &it : mp) {
+            if (!it.second.empty()) {
+                
+                for (auto val: it.second) {
+                    result.push_back(val);
+                }
+            }
+        }
+        return result;
+    }
+};
+
+*/
